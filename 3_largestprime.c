ef7e2f7db1c1a6df6311d2b31d6f0e71a346c96b
anerdsmeltybrain <hdviews.01@gmail.com>
2024-10-26 09:31:04 -0400

    solved question 3

---
+++ 3_largestprime.c
@@ -0,0 +1,32 @@
+#include "stdio.h"
+
+int main() {
+  long prime = 600851475143;
+  long result = 0;
+  int temp = 0;
+
+  for (int i = 2; i < 1000000; i++) {
+    if ( prime % i == 0) {
+      result = prime / i;
+      prime = result;
+      printf("%d\n", i);
+    }
+  }
+
+again:
+  for (int i = 2; i < 1000000; i++) {
+    if ( prime % i == 0) {
+      result = prime / i;
+      prime = result;
+      printf("%d\n", i);
+      goto again;
+    } else {
+      goto done;
+    }
+  }
+
+done:
+
+  printf("Program Done!!!");
+   return 0;
+}
